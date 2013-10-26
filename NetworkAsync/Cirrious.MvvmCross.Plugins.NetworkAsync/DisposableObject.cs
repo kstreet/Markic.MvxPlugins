506d7802793ba30573a4fde369cbd51eef4decd9
Kerry Street <kerrycstreet@gmail.com>
2013-10-26 11:56:55 -0700

    Task Extensions and support files - builds

---
+++ NetworkAsync/Cirrious.MvvmCross.Plugins.NetworkAsync/DisposableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cirrious.MvvmCross.Plugins.NetworkAsync
+{
+    public abstract class DisposableObject : IDisposable
+    {
+        protected bool disposed;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public bool IsDisposed
+        {
+            get { return disposed; }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            disposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ NetworkAsync/Cirrious.MvvmCross.Plugins.NetworkAsync/TaskFactoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cirrious.MvvmCross.Plugins.NetworkAsync
+{
+    public static class TaskFactoryExtensions
+    {
+        public static async Task<TResult> FromAsync<TResult, TSource>(this TaskFactory factory,
+            Func<AsyncCallback, object, IAsyncResult> beginMethod, Func<IAsyncResult, TResult> endMethod, TSource state, Action<TSource> abortMethod, CancellationToken ct)
+        {
+            using (CancellationTokenRegistration register = ct.Register(() => abortMethod(state)))
+            {
+                return await factory.FromAsync(beginMethod, endMethod, state);
+            }
+        }
+    }
+}
